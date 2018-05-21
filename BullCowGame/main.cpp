c9ba407e45e12692623b092274c40e8044ebb628
Juri <23640912+jthommen@users.noreply.github.co>
2018-05-21 18:40:11 +0100

    Initial Commit

---
+++ BullCowGame/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  BullCowGame
+//
+//  Created by Juri on 5/21/18.
+//  Copyright © 2018 Aetherwings. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
